1399a81d17b42f483ed24fed0d4370e39ce68d57
CelesteRenteria <celeste.renteria.is@unipolidgo.edu.mx>
2022-07-18 08:01:59 -0500

    commit ya bien la api

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -1,18 +1,22 @@
+import 'package:flutter/material.dart';
 import "package:http/http.dart" as http;
 import "dart:convert";
 
-class MoviesProvider {
+class MoviesProvider extends ChangeNotifier {
   final String _apiKey = "ee0fda4b27c94ea0fbe93b064b76b0b0";
-  final String _baseUrl = "api.themoviedb.org";
-  final String _language = "es-Es";
+  final String _baseUrl = 'api.themoviedb.org';
+  final String _language = "es-ES";
 
-  ModiesProviders(){}
+ 
+  MoviesProvider(){
+    this.getMovies();
+  }
 
   getMovies() async{
-    var url = Uri.https(_baseUrl, '/3/movie/now_playing?',{
+    var url = Uri.https(_baseUrl,'/3/movie/now_playing',{
       'api_key':_apiKey,
     'language':_language,
-    'page':1
+    'page':'1'
     });
 
   final response = await http.get(url);
